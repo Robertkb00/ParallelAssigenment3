97b3d9d426444bf238ceb6acd97d8dad3c25ed01
Robertkb00 <84647818+Robertkb00@users.noreply.github.com>
2023-04-13 20:08:55 -0400

    Update main.cpp
    Added mutex system

--- main.cpp
+++ main.cpp
@@ -1,1 +1,22 @@
-mainline
+#include <iostream>
+#include <mutex>
+
+
+#define THREAD_COUNT 4
+#define NUM_GUESTS 500000
+
+std::mutex presents;
+
+std::unique_ptr<std::unordered_set<int>> generateUnorderedSet(int size)
+{
+  std::unique_ptr<std::vector<int>> vect = std::make_unique<std::vector<int>>();
+  
+   for (int i = 0; i < size; i++)
+        vect->push_back(i);
+  
+   unsigned int seed = std::chrono::system_clock::now().time_since_epoch().count();
+   std::shuffle(vec->begin(), vec->end(), std::default_random_engine(seed));
+
+   return std::make_unique<std::unordered_set<int>>(vec->begin(), vec->end());
+
+}  
